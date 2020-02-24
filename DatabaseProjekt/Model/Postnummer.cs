dadea91876d21ea6c6c9139aab16aa9f64ea764f
FrederikVil <frederik@rovi.dk>
2020-02-24 14:18:55 +0100

    Start

---
+++ DatabaseProjekt/Model/Postnummer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseProjekt.Model
+{
+    public class Postnummer
+    {
+        public int postnummerID { get; set; }
+        public int postnummer { get; set; }
+
+        private SqlConnection myConn;
+
+        public Postnummer (SqlConnection c)
+        {
+            myConn = c;
+        }
+
+        public void Save()
+        {
+            string query = "INSERT INTO Postnummer (Postnummer) VALUES (@postnr)";
+            myConn.Open();
+            SqlCommand cmd = new SqlCommand(query, myConn);
+            cmd.Parameters.AddWithValue("@postnr", postnummer);
+
+            cmd.ExecuteNonQuery();
+
+
+
+
+        }
+
+
+
+    }
+}
--- DatabaseProjekt/Program.cs
+++ DatabaseProjekt/Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Data.SqlClient;
+using DatabaseProjekt.Model;
 
 namespace DatabaseProjekt
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            SqlConnection c = GetConnection();
+            Postnummer zip = new Postnummer(c);
+            zip.postnummer = 2;
+            zip.Save();
+        }
+        static SqlConnection GetConnection()
+        {
+            
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+            {
+                InitialCatalog = "Dyr",
+                UserID = "Dyr",
+                Password = "admin",
+                DataSource = "localhost"
+            };
+
+            SqlConnection conn = new SqlConnection(builder.ConnectionString);
+
+          //  SqlConnection conn = new SqlConnection("Server=localhost; Database=Dyr; Uid=Dyr;Pwd=admin");
+
+            return conn;
         }
     }
 }
