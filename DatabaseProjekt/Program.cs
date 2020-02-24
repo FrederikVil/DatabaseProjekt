7ed142f787f46156e3db120a5eebff08a4243e5a
FrederikVil <frederik@rovi.dk>
2020-02-24 09:07:53 +0100

    Add project files.

---
+++ DatabaseProjekt/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DatabaseProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
